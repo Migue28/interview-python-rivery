0556baa21ae5f8b476d033f01908db67fbc10843
Eddy Cedeño <miguepoke28@gmail.com>
2023-03-04 15:45:23 -0500

    get news api working

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+import os
+import rivery_cli
+
 
 def get_news_data(api_key):
     """
@@ -6,7 +13,14 @@ def get_news_data(api_key):
     :param api_key: The API key to use for the NewsAPI.org API.
     :return: A list of dictionaries containing the most popular news from the previous day.
     """
-
+    yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+    url = f"https://newsapi.org/v2/everything?q=*&sortBy=popularity&from={yesterday}"
+    headers = {"Authorization": "Bearer " + api_key}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 def insert_news(news, connection):
     """
@@ -17,6 +31,9 @@ def insert_news(news, connection):
     """
 
 
+load_dotenv()
+api_key = os.environ.get("NEWS_API_KEY")
+
 if __name__ == '__main__':
-    api_key = ""
-    get_news_data(api_key)
+    api_key = api_key
+    print(get_news_data(api_key))
